13f054396061eef66fa31a2f945260019d56f428
hxy <455979052@qq.com>
2018-06-30 23:33:07 +0800

    sss

--- conf.js
+++ conf.js
@@ -1,3 +1,3 @@
 var libpath = "http://124.133.27.90:6081/arcgis_js_api3.18/library/3.18/3.18";
 var fsurl = 'http://124.133.27.90:6080/arcgis/rest/services/sdbj/FeatureServer/';
-var serveraddr = "http://localhost:800/";
+var serveraddr = "http://localhost:6083/";
