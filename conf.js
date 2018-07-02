234231ea1b5b379471cec3bbf82382352ef37191
hxylongao <455979052@qq.com>
2018-07-02 15:33:07 +0800

    sss

--- conf.js
+++ conf.js
@@ -1,3 +1,4 @@
 var libpath = "http://124.133.27.90:6081/arcgis_js_api3.18/library/3.18/3.18";
+// libpath = "https://js.arcgis.com/3.24/";
 var fsurl = 'http://124.133.27.90:6080/arcgis/rest/services/sdbj/FeatureServer/';
 var serveraddr = "http://localhost:6083/";
